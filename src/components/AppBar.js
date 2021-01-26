d85dad2880d629599ce11e2408d013827f370b70
Jhoneagle <eaglekjohn@gmail.com>
2021-01-26 20:02:43 +0200

    signIn view added and appBar updated

--- src/components/AppBar.js
+++ src/components/AppBar.js
@@ -1,19 +1,23 @@
 import React from 'react';
-import { View, StyleSheet, TouchableWithoutFeedback } from 'react-native';
+import { View, StyleSheet, TouchableWithoutFeedback, ScrollView } from 'react-native';
+import { Link } from 'react-router-native';
 import Constants from 'expo-constants';
+
 import Text from "./Text";
 import theme from "../theme";
 
 const styles = StyleSheet.create({
-  container: {
+  flexContainer: {
     backgroundColor: theme.colors.barBackground,
     display: theme.adjustment.flexible,
     flexDirection: theme.adjustment.toColumns,
-    justifyContent: 'space-around',
-    paddingTop: Constants.statusBarHeight * 2,
-    paddingRight: Constants.statusBarHeight * 0.75,
+    justifyContent: 'center',
+    paddingTop: Constants.statusBarHeight,
     paddingBottom: Constants.statusBarHeight,
-    paddingLeft: Constants.statusBarHeight * 0.75,
+  },
+  contentContainer: {
+    flexGrow: 1,
+    justifyContent: 'space-between',
   },
   text: {
     color: theme.colors.barText,
@@ -24,10 +28,19 @@ const styles = StyleSheet.create({
 const AppBar = () => {
   return (
     <TouchableWithoutFeedback>
-      <View style={styles.container}>
-        <Text style={styles.text}>
-          Repositories
-        </Text>
+      <View style={styles.flexContainer}>
+        <ScrollView horizontal contentContainerStyle={styles.contentContainer}>
+          <Link to="/">
+            <Text fontSize="subheading" fontWeight="bold" style={styles.text}>
+              Repositories
+            </Text>
+          </Link>
+          <Link to="/signIn">
+            <Text fontSize="subheading" fontWeight="bold" style={styles.text}>
+              Sign in
+            </Text>
+          </Link>
+        </ScrollView>
       </View>
     </TouchableWithoutFeedback>
   );
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import { StyleSheet, View } from 'react-native';
+import { Route, Switch, Redirect } from 'react-router-native';
+
 import RepositoryList from "./Repository/RepositoryList";
 import AppBar from "./AppBar";
+import SignIn from "./SignIn";
 
 const styles = StyleSheet.create({
     container: {
         display: 'flex',
+        flexGrow: 1,
+        flexShrink: 1,
     },
 });
 
@@ -13,7 +18,15 @@ const Main = () => {
     return (
         <View style={styles.container}>
             <AppBar />
-            <RepositoryList />
+            <Switch>
+              <Route path="/" exact>
+                <RepositoryList />
+              </Route>
+              <Route path="/signIn" exact>
+                <SignIn />
+              </Route>
+              <Redirect to="/" />
+            </Switch>
         </View>
     );
 };
